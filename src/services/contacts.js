06af6ce442c66f9ed9e485e023c16b1b7b24f870
vladvolchek <mskoresh19@gmail.com>
2024-09-05 19:29:48 +0300

    fix

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,14 +1,5 @@
 import { ContactsCollection } from '../db/models/contacts.js';
 
-// export const getAllContacts = async () => {
-//   const contacts = await ContactsCollection.find();
-//   return contacts;
-// };
-
-// export const getContactById = async (id) => {
-//   const contact = await ContactsCollection.findById(id);
-//   return contact;
-// };
 
 export const getAllContacts = () => ContactsCollection.find();
 
